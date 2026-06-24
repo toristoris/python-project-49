1d23b7296ee4838a27280f8d59a1bbb9eecf1ec8
toristoris <sokolenkov0@gmail.com>
2026-06-24 16:16:26 +0300

    Modified brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,15 @@
 from brain_games.games.brain_even import MAX_TRIES, RULES, check_answer, get_question
 from brain_games.engine import game_engine
 
-def main(
+
+def main():
+    game_engine(
         rules=RULES,
         max_tries=MAX_TRIES,
         func_question=get_question,
         check_answer=check_answer,
-):
+    )
+
 
 if __name__ == "__main__":
     main()
