7e2dfaff3b0b71306717eb4c4304c70fabc6cff4
toristoris <sokolenkov0@gmail.com>
2026-06-24 18:15:18 +0300

    Make script brain_gcd.py

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,14 @@
+from brain_games.games.brain_gcd import MAX_TRIES, RULES, get_question
+from brain_games.engine import game_engine
+
+
+def main():
+    game_engine(
+        rules=RULES,
+        max_tries=MAX_TRIES,
+        func_question=get_question,
+    )
+
+
+if __name__ == "__main__":
+    main()
