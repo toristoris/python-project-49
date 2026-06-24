5d1633c2daf271e7122989044845f44b47000686
toristoris <sokolenkov0@gmail.com>
2026-06-24 18:18:34 +0300

    Fix

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -6,6 +6,7 @@ RULES = (
     'Answer "yes" if the number is even, otherwise answer "no".'  # знакомим с правилами
 )
 
+
 def get_question():
     number = random.randint(1, 100)
     question = number
