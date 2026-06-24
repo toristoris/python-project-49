e98c2970a69d41542a7e09095b7e22b562e115cb
toristoris <sokolenkov0@gmail.com>
2026-06-24 18:06:18 +0300

    Fix brain_gcd.py

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -11,6 +11,5 @@ def get_question():
     a = random.randint(1, 100)
     b = random.randint(1, 100)
     correct_answer = math.gcd(a, b) 
-
     question = f"{a} {b}"
     return question, correct_answer
